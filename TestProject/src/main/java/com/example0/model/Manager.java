e3e5fe2451da2e55f1333207c4d57ec1cf44333c
acto11 <js23zzang@naver.com>
2022-05-18 09:43:37 +0900

    Manager, reservation 테이블추가

--- TestProject/src/main/java/com/example0/model/Manager.java
+++ TestProject/src/main/java/com/example0/model/Manager.java
@@ -1,5 +1,33 @@
 package com.example0.model;
 
-public class Manager {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "manager")
+public class Manager {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int mnum;
+	@Column(nullable = false)
+	private String m_name;
+	private String m_call;
+	private String m_cname;
+	private String m_email;
+	private String m_pwd;
+	private int m_age;
+	private String cname;
+	
 }
