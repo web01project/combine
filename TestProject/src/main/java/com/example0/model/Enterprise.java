add4198f83bc33f9172bfc006ba7cdbcfa52a752
mini1115 <kmini9611@naver.com>
2022-05-18 10:46:11 +0900

    기본 폴더 설정

--- TestProject/src/main/java/com/example0/model/Enterprise.java
+++ TestProject/src/main/java/com/example0/model/Enterprise.java
@@ -2,8 +2,6 @@ package com.example0.model;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
