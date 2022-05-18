c1de7fab2f38216c7adad62ef8813633baee1d9f
kim95 <dlsxj2188@gmail.com>
2022-05-18 09:43:30 +0900

    리뷰 유저

--- TestProject/src/main/java/com/example0/model/Review.java
+++ TestProject/src/main/java/com/example0/model/Review.java
@@ -1,5 +1,42 @@
 package com.example0.model;
 
-public class Review {
+import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import org.hibernate.annotations.CreationTimestamp;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@Entity
+@Table
+public class Review {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long review_id;
+	private String content;
+	
+	@CreationTimestamp
+	@Temporal(TemporalType.TIMESTAMP)
+	@JsonFormat(pattern = "yyyy-MM-dd")
+	private Date regdate;
+	private int point;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+	@ManyToOne
+	@JoinColumn(name = "hotel_id")
+	private Manager manager;
 }
