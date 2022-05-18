9ab85109cefa946ce02e7ed8630e85fe364b35ac
acto11 <js23zzang@naver.com>
2022-05-18 10:13:04 +0900

    manager, reservation 주석추가

--- TestProject/src/main/java/com/example0/model/Manager.java
+++ TestProject/src/main/java/com/example0/model/Manager.java
@@ -22,12 +22,18 @@ public class Manager {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int mnum;
 	@Column(nullable = false)
+	//이름
 	private String m_name;
+	//전화번호
 	private String m_call;
+	//회사명
 	private String m_cname;
+	//이메일
 	private String m_email;
+	//비밀번호
 	private String m_pwd;
+	//나이
 	private int m_age;
+	//회사이름
 	private String cname;
-	//
 }
--- TestProject/src/main/java/com/example0/model/Reservation.java
+++ TestProject/src/main/java/com/example0/model/Reservation.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -19,10 +21,21 @@ import lombok.Setter;
 public class Reservation {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	//주문번호
 	private String or_num;
+	//회원번호
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private String user_id;
+	//숙소번호
+	@ManyToOne
+	@JoinColumn(name = "")
 	private String room_num;
+	//인원
 	private int people;
+	//체크인시간
 	private String check_in;
+	//체크아웃시간
 	private String check_out;
-	//
+
 }
