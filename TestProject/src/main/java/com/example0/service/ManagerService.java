afb8eba4237b8d6d7208352dd4a0142d08d76c66
kim95 <dlsxj2188@gmail.com>
2022-05-19 12:04:08 +0900

    commit

--- TestProject/src/main/java/com/example0/service/ManagerService.java
+++ TestProject/src/main/java/com/example0/service/ManagerService.java
@@ -3,6 +3,7 @@ package com.example0.service;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.example0.model.Enterprise;
 import com.example0.model.Manager;
 import com.example0.repository.EnterpriseRepository;
 import com.example0.repository.ManagerRepository;
@@ -16,11 +17,12 @@ public class ManagerService {
 	private final ManagerRepository managerRepository;
 	private final EnterpriseRepository enterpriseRepository;
 	private final BCryptPasswordEncoder encoder;
+	private Enterprise enterprise;
 	
 	//회원가입
 	public void managerjoin(Manager manager) {
 		System.out.println(manager);
-		enterpriseRepository.findById(null);
+//		enterpriseRepository.findById(null);
 		
 		String rawPassword = manager.getM_password();
 		String encPassword = encoder.encode(rawPassword);
