c13f9403438bdefed7cef702b304a9f86095f887
mini1115 <kmini9611@naver.com>
2022-05-24 10:33:59 +0900

    숙소 리스트 수정

--- TestProject/src/main/java/com/example0/repository/BoardRepository.java
+++ TestProject/src/main/java/com/example0/repository/BoardRepository.java
@@ -1,9 +1,16 @@
 package com.example0.repository;
 
+import java.util.HashMap;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example0.model.Hotel;
 
 public interface BoardRepository extends JpaRepository<Hotel, Long> {
-
+	
+	@Query("select count(*) from Hotel")
+	public int getCount(HashMap<String, Object> map);
+	
+	
 }
