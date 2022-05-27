4b4c25d4232eb90ee5c967ef0b66d777aa8c2e81
acto11 <js23zzang@naver.com>
2022-05-27 17:02:16 +0900

    커밋

--- TestProject/src/main/java/com/example0/controller/BoardController.java
+++ TestProject/src/main/java/com/example0/controller/BoardController.java
@@ -148,7 +148,7 @@ private ReservationRepository reservationRepository;
 		model.addAttribute("hotel", boardService.findById(h_num) );
 		return "/hotel/hotelview";
 	}
-	//삭제
+	//숙소삭제
 	@DeleteMapping("delete/{num}")
 	@ResponseBody
 		public String delete(@PathVariable Long num) {
