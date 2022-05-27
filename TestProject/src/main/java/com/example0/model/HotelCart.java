485c93585844618edae21a228885272f53e08483
kim95 <dlsxj2188@gmail.com>
2022-05-27 14:12:25 +0900

    커밋

---
+++ TestProject/src/main/java/com/example0/model/HotelCart.java
@@ -0,0 +1,19 @@
+package com.example0.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+//@Getter@Setter
+//@AllArgsConstructor @NoArgsConstructor
+//@Entity
+public class HotelCart {
+//	@Id
+//	@GeneratedValue(strategy = )
+//	private Long 
+}
